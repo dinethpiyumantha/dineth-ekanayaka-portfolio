09c20fd5f03ebb08d12de6d29c3e66e065e83677
dinethpiyumantha <dinethpiyumantha@gmail.com>
2021-11-09 13:49:19 +0530

    contact form created

--- src/views/contact/Contact.js
+++ src/views/contact/Contact.js
@@ -1,12 +1,54 @@
 import React from "react";
-import RichTextEditor from "./componets/RichTextEditor";
 import "./style.css";
 
 export default function Contact() {
   return (
     <div>
-      <h1>Contact</h1>
-      <RichTextEditor />
+      <form className="d-flex justify-content-center">
+        <div className="card p-4 col-md-5">
+          <h2>Get in touch with me</h2>
+          <div className="mb-3">
+            <label for="exampleFormControlInput1" className="form-label">
+              Name
+            </label>
+            <input
+              v-model="item.name"
+              type="text"
+              className="form-control"
+              id="exampleFormControlInput1"
+              name="user_name"
+            />
+          </div>
+          <div className="mb-3">
+            <label for="exampleFormControlInput1" className="form-label">
+              Email Address
+            </label>
+            <input
+              v-model="item.email"
+              type="email"
+              className="form-control"
+              id="exampleFormControlInput1"
+              placeholder="name@example.com"
+              name="user_email"
+            />
+          </div>
+          <div className="mb-3">
+            <label for="exampleFormControlTextarea1" className="form-label">
+              Message
+            </label>
+            <textarea
+              v-model="item.message"
+              className="form-control"
+              id="exampleFormControlTextarea1"
+              rows="6"
+              name="message"
+            ></textarea>
+          </div>
+          <button type="submit" className="btn btn-dp mb-3 w-25">
+            Send
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
